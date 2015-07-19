587ba45b6f041cc8ef78b7c05ad2662e4f168e98
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-07-19 07:55:27 -0400

    second kind of fold

--- src/tagtree.rs
+++ src/tagtree.rs
@@ -8,29 +8,55 @@ pub enum TagTree<Leaf, Node, Edge> {
 
 impl<Leaf, Node, Edge> TagTree<Leaf, Node, Edge> {
 
-    pub fn fold_tag_tree<T, FL, FN, FE> (
+    pub fn fold_tag_tree<T, FL, FN> (
+        &self,
+        ref reduce_leaf: FL,
+        ref reduce_node: FN) -> T
+        where
+        FL: Fn(&Leaf) -> T,
+        FN: Fn(&Node, Vec<(&Edge, T)>) -> T
+        {
+            match self {
+                &TagTree::Leaf(ref leaf) => reduce_leaf(leaf),
+                &TagTree::Node(ref node, ref children) => {
+                    let edge_subs = children.iter().map(|edge_child| {
+                        match edge_child {
+                            &(ref edge, box ref child) => {
+                                let sub = child.fold_tag_tree(reduce_leaf, reduce_node);
+                                (edge, sub)
+                            }
+                        }
+                    }).collect();
+                    reduce_node(node, edge_subs)
+                }
+            }
+        }
+
+
+
+    pub fn fold_tag_tree2<T, FL, FN, FE> (
         &self,
         ref reduce_leaf: FL,
         ref reduce_node: FN,
         ref reduce_edge: FE) -> T
         where 
-            FL: Fn(&Leaf) -> T, 
-            FN: Fn(&Node, Vec<T>) -> T, 
-            FE: Fn(&Edge, T) -> T 
-            {
-                match self {
-                    &TagTree::Leaf(ref leaf) => reduce_leaf(leaf),
-                    &TagTree::Node(ref node, ref children) => {
-                        let subs = children.iter().map(|edge_child| { 
-                            match edge_child {
-                                &(ref edge, box ref child) => {
-                                    let sub = child.fold_tag_tree(reduce_leaf, reduce_node, reduce_edge);    
-                                    reduce_edge(edge, sub)
-                                }
+        FL: Fn(&Leaf) -> T, 
+        FN: Fn(&Node, Vec<T>) -> T, 
+        FE: Fn(&Edge, T) -> T 
+        {
+            match self {
+                &TagTree::Leaf(ref leaf) => reduce_leaf(leaf),
+                &TagTree::Node(ref node, ref children) => {
+                    let subs = children.iter().map(|edge_child| { 
+                        match edge_child {
+                            &(ref edge, box ref child) => {
+                                let sub = child.fold_tag_tree2(reduce_leaf, reduce_node, reduce_edge);    
+                                reduce_edge(edge, sub)
                             }
-                        }).collect();
-                        reduce_node(node, subs)
-                    }
+                        }
+                    }).collect();
+                    reduce_node(node, subs)
                 }
             }
+        }
 }
