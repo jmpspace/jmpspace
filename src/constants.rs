4b0fa2551040e0f0df78e0a6ff0a26da13ef7155
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-07-21 18:53:41 -0400

    better test examples

--- src/constants.rs
+++ src/constants.rs
@@ -3,4 +3,7 @@ pub const VESSEL_DENSITY: f64 = 0.1;
 pub const FUELTANK_DENSITY: f64 = 1.0;
 pub const ENGINE_DENSITY: f64 = 0.3;
 
+pub const BEAM_WIDTH: f64 = 0.2;
+pub const BEAM_DENSITY: f64 = 0.6;
+
 pub const ENGINE_THRUST: f64 = 0.2;
--- src/ship.rs
+++ src/ship.rs
@@ -1,15 +1,15 @@
 
 use na::{Vec2};
-use ncollide::shape::{Cone, Cuboid};
+use ncollide::shape::{Cone, Cuboid, Cylinder};
 use nphysics::math::{Matrix, Point};
 use nphysics::object::{RigidBody};
 
-use constants::{VESSEL_DENSITY, FUELTANK_DENSITY, ENGINE_DENSITY};
+use constants::*;
 use tagtree;
 
 enum Part {
   Vessel { width: f64, length: f64 },
-  FuelTank { width: f64, length: f64 },
+  FuelTank { radius: f64, length: f64 },
   Engine { radius: f64, length: f64, group: i32 }
 }
 
@@ -34,8 +34,8 @@ impl Part {
                 let geom = Cuboid::new(Vec2::new(width, length));
                 RigidBody::new_dynamic(geom, VESSEL_DENSITY, 1.0, 1.0)
             }
-            &Part::FuelTank {width, length} => {
-                let geom = Cuboid::new(Vec2::new(width, length));
+            &Part::FuelTank {radius, length} => {
+                let geom = Cylinder::new(length, radius);
                 RigidBody::new_dynamic(geom, FUELTANK_DENSITY, 1.0, 1.0)
             }
             &Part::Engine {radius, length, group} => {
@@ -54,6 +54,15 @@ struct PartObjectCache {
 
 struct Beam {
     length: f64
+}
+
+impl Beam {
+
+    fn object (&self) -> RigidBody {
+        let geom = Cuboid::new(Vec2::new(BEAM_WIDTH, self.length));
+        RigidBody::new_dynamic(geom, BEAM_DENSITY, 1.0, 1.0)
+    }
+
 }
 
 struct Attach {
@@ -78,11 +87,19 @@ fn beam(length: f64, parts: Vec<(Attach, Box<Structure>)>) -> Structure {
 #[test]
 fn simple_parts () {
     part(Part::Vessel { width: 2.0, length: 4.0 });
-    part(Part::FuelTank { width: 1.0, length: 5.0});
-    part(Part::Engine { width: 1.0, length: 2.0, group: 3});
-    beam(5.0, Vec::new());
+    part(Part::FuelTank { radius: 1.0, length: 5.0});
+    part(Part::Engine { radius: 1.0, length: 2.0, group: 3});
+    beam(5.0, vec![]);
 }
 
 #[test]
-fn compount_ships () {
+fn simple_ships () {
+    beam(8.0, vec![
+         (Attach {location: 2.0, rotation: 0.0}, 
+          box part(Part::Vessel {width: 2.0, length: 4.0})),
+          (Attach {location: 6.0, rotation: 0.0},
+          box part(Part::FuelTank {radius: 1.0, length: 1.5})),
+          (Attach {location: 8.0, rotation: 0.0},
+          box part(Part::Engine {radius: 1.5, length: 0.5, group: 1}))
+         ]);
 }
