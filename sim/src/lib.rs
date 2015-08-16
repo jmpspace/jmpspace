9a7e8ee00356f2cd791f5d6d7756a83cc33bde3a
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-08-16 02:51:30 -0400

    checkpoint - pointers, box, lifetimes, etc. what's this going to look like in memory

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -2,6 +2,8 @@
 #![feature(box_patterns)]
 #![feature(box_syntax)]
 
+#![allow(dead_code)]
+
 extern crate nalgebra as na;
 extern crate ncollide;
 extern crate nphysics;
--- sim/src/ship.rs
+++ sim/src/ship.rs
@@ -1,32 +1,38 @@
 
 use na::{Vec2};
 use ncollide::shape::{Cone, Cuboid, Cylinder};
-use nphysics::math::{Matrix, Point};
 use nphysics::object::{RigidBody};
 
 use constants::*;
 use tagtree;
 
 enum Part {
-  Vessel { width: f64, length: f64 },
-  FuelTank { radius: f64, length: f64 },
-  Engine { radius: f64, length: f64, group: i32 }
+    Vessel { width: f64, length: f64 },
+    FuelTank { radius: f64, length: f64 },
+    Engine { radius: f64, length: f64, group: i32 }
+}
+
+struct PointMass {
+    center: Vec2<f64>,
+    mass: f64
 }
 
 impl Part {
 
-    /*
     fn mass (&self) -> f64 {
         match self {
-            &Part::Vessel {width, length} => 
-                VESSEL_DENSITY * width * length,
-            &Part::FuelTank {width, length} =>
-                FUELTANK_DENSITY * width * length,
-            &Part::Engine {radius, length, group} =>
+            &Part::Vessel {width, length} => {
+                VESSEL_DENSITY * width * length
+            }
+            &Part::FuelTank {radius, length} => {
+                FUELTANK_DENSITY * radius * length
+            }
+            &Part::Engine {radius, length, group} => {
+                let _group = group;
                 ENGINE_DENSITY * radius * length * 0.5
+            }
         }
     }
-    */
 
     fn object (&self) -> RigidBody {
         match self {
@@ -39,11 +45,29 @@ impl Part {
                 RigidBody::new_dynamic(geom, FUELTANK_DENSITY, 1.0, 1.0)
             }
             &Part::Engine {radius, length, group} => {
+                let _group = group;
                 let geom = Cone::new(length, radius);
                 RigidBody::new_dynamic(geom, ENGINE_DENSITY, 1.0, 1.0)
             }
         }
     }
+
+    fn point_mass (&self) -> PointMass {
+        PointMass { center: Vec2::new(0.0,0.0), mass: self.mass() }
+    }
+
+}
+
+#[test]
+fn simple_parts () {
+
+    let p1 = Part::Vessel { width: 2.0, length: 4.0 };
+    let _p2 = Part::FuelTank { radius: 1.0, length: 5.0};
+    let _p3 = Part::Engine { radius: 1.0, length: 2.0, group: 3};
+
+    let _m1 = p1.mass();
+    let _pm1 = p1.point_mass();
+
 }
 
 struct PartObjectCache {
@@ -62,6 +86,15 @@ impl Beam {
         let geom = Cuboid::new(Vec2::new(BEAM_WIDTH, self.length));
         RigidBody::new_dynamic(geom, BEAM_DENSITY, 1.0, 1.0)
     }
+    
+    fn mass (&self) -> f64 {
+        self.length * BEAM_DENSITY
+    }
+
+    fn point_mass (&self) -> PointMass {
+        let midpoint = self.length / 2.0;
+        PointMass { center: Vec2::new(midpoint, 0.0), mass: self.mass() }
+    }
 
 }
 
@@ -73,7 +106,25 @@ struct Attach {
 type Structure = tagtree::TagTree<Part,Beam,Attach>;
 
 impl Structure {
-    
+
+    fn part_masses(part: &Part) -> Vec<PointMass> {
+        // Performance ?
+        let mut v = Vec::new();
+        v.push(part.point_mass());
+        v
+    }
+
+    fn beam_masses<'a>(beam: &'a Beam, subs: Vec<&'a Vec<&'a PointMass>>) -> Vec<&'a PointMass> {
+        let mut v = Vec::new();
+        v.push(beam.point_mass());
+        // TODO push all
+        v
+    }
+
+    fn attach_masses<'a>(attach: &'a Attach, subs: Vec<&'a PointMass>) -> Vec<&'a PointMass> {
+        subs
+    }
+
 }
 
 fn part(attrs: Part) -> Structure {
@@ -83,9 +134,9 @@ fn part(attrs: Part) -> Structure {
 fn beam(length: f64, parts: Vec<(Attach, Box<Structure>)>) -> Structure {
     tagtree::TagTree::Node(Beam {length: length}, parts)
 }
-
+ 
 #[test]
-fn simple_parts () {
+fn simple_structures () {
     part(Part::Vessel { width: 2.0, length: 4.0 });
     part(Part::FuelTank { radius: 1.0, length: 5.0});
     part(Part::Engine { radius: 1.0, length: 2.0, group: 3});
@@ -98,8 +149,8 @@ fn simple_ships () {
          (Attach {location: 2.0, rotation: 0.0}, 
           box part(Part::Vessel {width: 2.0, length: 4.0})),
           (Attach {location: 6.0, rotation: 0.0},
-          box part(Part::FuelTank {radius: 1.0, length: 1.5})),
-          (Attach {location: 8.0, rotation: 0.0},
-          box part(Part::Engine {radius: 1.5, length: 0.5, group: 1}))
-         ]);
+           box part(Part::FuelTank {radius: 1.0, length: 1.5})),
+           (Attach {location: 8.0, rotation: 0.0},
+            box part(Part::Engine {radius: 1.5, length: 0.5, group: 1}))
+           ]);
 }
