0307b756890a1d315f0c6829bfdde4f3f065c7d6
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-09-09 21:04:40 -0400

    cleanup prep for closing the action loop

--- server/main.go
+++ server/main.go
@@ -22,7 +22,6 @@ func main() {
 	snapshot_world(sim)
 
 	http.Handle("/", http.FileServer(http.Dir("client/site")))
-	http.Handle("/chat", websocket.Handler(chatServer()))
 	http.Handle("/action", websocket.Handler(actionServer()))
 
 	log.Print("Starting server on port 8080")
