f52cbd3fb6d53d175d8df527dade8b225adeb608
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-09-30 22:52:12 -0400

    remove FFI from golang

--- server/main.go
+++ server/main.go
@@ -19,51 +19,9 @@ type Connect struct {
 
 func main() {
 
-	hello_sim()
-	sim := build_world()
-
 	connects := make(chan Connect)
-	ticks := make(chan interface{})
 	actions := make(chan Action)
 	snapshots := make(chan Snapshot)
-
-	go func() {
-
-		for {
-
-			select {
-
-			case connect := <-connects:
-				log.Print("Starting connect")
-				entity := connect_client(sim, connect.client)
-				connect.finished <- entity
-				log.Print("Finished connect")
-
-			case <-ticks:
-				log.Print("Starting tick")
-				update_world(sim)
-				buf := snapshot_world(sim)
-				snapshots <- Snapshot{buf: buf}
-				log.Print("Finished tick")
-
-			case action := <-actions:
-				log.Print("Starting action")
-				apply_action(sim, action.client, action.buf)
-				log.Print("Finished action")
-			}
-
-		}
-
-	}()
-
-	/*
-		go func() {
-			for {
-				time.Sleep(50 * time.Millisecond)
-				ticks <- nil
-			}
-		}()
-	*/
 
 	http.Handle("/", http.FileServer(http.Dir("site")))
 	http.Handle("/action", websocket.Handler(actionServer(connects, actions, snapshots)))
