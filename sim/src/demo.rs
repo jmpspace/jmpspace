bca71e199baa199a748b4fc3b3ae2eac4fff66fb
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-12-15 01:26:56 -0500

    more macro conversion

--- sim/src/demo.rs
+++ sim/src/demo.rs
@@ -5,6 +5,8 @@ use std::f64::consts::PI;
 use ship::{Attach, Beam, Part, Structure};
 use tagtree;
 
+// TODO can further simplify with the builder macros
+
 pub fn simple_ship() -> Structure {
 
     let (forward, reverse, left, right) = (0,1,2,3);
@@ -26,12 +28,12 @@ pub fn simple_ship() -> Structure {
 
     let mods = vec!(
         attach_tree!(6.0, 3.0 * PI / 2.0, rev_l),
-        (Attach { location: 6.0, rotation: PI / 2.0}, box rev_r),
-        (Attach { location: 42.0, rotation: 3.0 * PI / 2.0 }, box attitude_l),
-        (Attach { location: 42.0, rotation: PI / 2.0}, box attitude_r),
-		attach_part!(0.0, 0.0, vessel!(7.0, 7.0)),
-        (Attach { location: 25.0, rotation: 0.0}, box part!(fuel_tank!(14.0, 30.0))),
-        (Attach { location: 50.0, rotation: 0.0}, box part!(engine!(13.0, 13.0, forward)))
+        attach_tree!(6.0, PI / 2.0, rev_r),
+        attach_tree!(42.0, 3.0 * PI / 2.0, attitude_l),
+        attach_tree!(42.0, PI / 2.0, attitude_r),
+        attach_part!(0.0, 0.0, vessel!(7.0, 7.0)),
+        attach_part!(25.0, 0.0, fuel_tank!(14.0, 30.0)),
+        attach_part!(50.0, 0.0, engine!(13.0, 13.0, forward)),
         );
 
     beam!(50.0, mods)
